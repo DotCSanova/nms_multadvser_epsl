32030bf35efe3010181c041747af38290648dadc
DotCSanova <albertolopezcasanova@icloud.com>
2025-03-21 12:03:33 +0100

    actualizar docker compose para tener como volumen el codigo local

--- web-server/src/index.js
+++ web-server/src/index.js
@@ -22,7 +22,7 @@ app.use('/api/auth', authRoutes);
 //app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
-    res.send('sERVIDOR fUNCIONANDO!');
+    res.send('sERVIDOR fUNCIONANDO bien!');
     });
 // Iniciar servidor
 app.listen(PORT, () => {
